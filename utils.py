18b45bde0e63a88b7dad9208c12ab1d361c6693d
paulsilvap <paul.silva@yachaytech.edu.ec>
2022-04-05 18:00:43 +0900

    add weights to graph

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import networkx as nx
 
-def create_graph(row, column):
+def create_graph(row, column, weight_limit=2):
 
     matrix = np.reshape(np.array([x for x in range(column*row)]),(column,row))
 
@@ -10,13 +10,12 @@ def create_graph(row, column):
     for i in range(len(matrix)):
         for j in range(len(matrix)):
             if j+1 < len(matrix):
-                graph.add_edge(matrix[i][j], matrix[i][j+1])
+                graph.add_edge(matrix[i][j], matrix[i][j+1], state = np.random.randint(1,weight_limit))
             if i+1 < len(matrix):
-                graph.add_edge(matrix[i][j], matrix[i+1][j])
+                graph.add_edge(matrix[i][j], matrix[i+1][j], state = np.random.randint(1,weight_limit))
             if j-1 >= 0:
-                graph.add_edge(matrix[i][j], matrix[i][j-1])
+                graph.add_edge(matrix[i][j], matrix[i][j-1], state = np.random.randint(1,weight_limit))
             if i-1 >= 0:
-                graph.add_edge(matrix[i][j], matrix[i-1][j])
+                graph.add_edge(matrix[i][j], matrix[i-1][j], state = np.random.randint(1,weight_limit))
     
     return graph
-
